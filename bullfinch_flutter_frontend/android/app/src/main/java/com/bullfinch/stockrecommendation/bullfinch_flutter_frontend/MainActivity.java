705e3c8b47c98adf2e8af851d97733d6c9e6a02d
Loke <92522067+LokeshSelvakumar@users.noreply.github.com>
2022-03-22 10:27:54 +0000

    undo creation

--- bullfinch_flutter_frontend/android/app/src/main/java/com/bullfinch/stockrecommendation/bullfinch_flutter_frontend/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.bullfinch.stockrecommendation.bullfinch_flutter_frontend;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
