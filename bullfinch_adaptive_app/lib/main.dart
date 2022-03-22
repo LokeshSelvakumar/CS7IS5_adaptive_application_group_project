ebd7526a9d6b536d5610b8a957bcc320c1960223
Loke <92522067+LokeshSelvakumar@users.noreply.github.com>
2022-03-22 10:45:25 +0000

    temporary login screen implementation

--- bullfinch_adaptive_app/lib/main.dart
+++ bullfinch_adaptive_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Routes/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,11 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+     // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (context) => loginScreen(),
+      },
     );
   }
 }
