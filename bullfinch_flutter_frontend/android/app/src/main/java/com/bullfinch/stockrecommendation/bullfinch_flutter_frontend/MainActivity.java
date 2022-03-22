8a9b15d830195fee82e233ca020ffdbcfc812ff9
Loke <92522067+LokeshSelvakumar@users.noreply.github.com>
2022-03-22 10:19:18 +0000

    bullfinch flutter initial commit

---
+++ bullfinch_flutter_frontend/android/app/src/main/java/com/bullfinch/stockrecommendation/bullfinch_flutter_frontend/MainActivity.java
@@ -0,0 +1,6 @@
+package com.bullfinch.stockrecommendation.bullfinch_flutter_frontend;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
