c1bf0fca512cfd5f460a24484d3f3f8994e6eee6
Loke <92522067+LokeshSelvakumar@users.noreply.github.com>
2022-03-26 16:04:42 +0000

    sign up initial screen commit

--- bullfinch_adaptive_app/lib/main.dart
+++ bullfinch_adaptive_app/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'Routes/homePage.dart';
 import 'Routes/login.dart';
+import 'Routes/signUpScreen.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -12,25 +20,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-     // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/': (context) => loginScreen(),
-      },
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        initialRoute: '/',
+        routes: <String, WidgetBuilder>{
+          '/': (context) => loginScreen(),
+          '/homePage': (context) => homePage(),
+          '/signUp': (context) => signUpScreen(),
+        });
   }
 }
 
