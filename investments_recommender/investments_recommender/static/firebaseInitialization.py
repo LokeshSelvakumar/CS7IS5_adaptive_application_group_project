ad8ad4523c71c8253b729e7fa5b42b2f8890aafa
KiserSlay <1171765982@qq.com>
2022-04-20 15:28:38 +0100

    Add code for storing user information and user preferences

--- investments_recommender/investments_recommender/static/firebaseInitialization.py
+++ investments_recommender/investments_recommender/static/firebaseInitialization.py
@@ -10,16 +10,32 @@ cred_obj = credentials.Certificate(privPath)
 default_app = firebase_admin.initialize_app(cred_obj, {
     'databaseURL': 'https://investment-recommender-default-rtdb.europe-west1.firebasedatabase.app/'
 })
-ref = db.reference("/")
-ref.set({"Users": {
-    "user_id1": {
-        "Name": "Jack",
-        "Username": "jack"
-    }
-}})
-ref.update({"Users": {
-    "user_id2": {
-        "Name": "Lucy",
-        "Username": "lucy"
-    }
-}})
+# ref = db.reference("/Users")
+# ref = db.reference("/Reference")
+
+def save_user(user_id, name, age, gender, email, occupation, salary):
+    ref = db.reference("/Users")
+    ref.update({
+        user_id: {
+            "Name": name,
+            "Age":  age,
+            "Gender": gender,
+            "Email": email,
+            "Occupation": occupation,
+            "Salary Range": salary
+        }
+    })
+
+def save_reference(user_id, reference, knowledge, news, stock):
+    ref = db.reference("/Reference")
+    ref.update({
+        user_id:{
+            "reference field": reference,
+            "knowledge": knowledge,
+            "news" : news,
+            "stock": stock
+        }
+    })
+save_user("12345", "KiKi", 23, "Female", "123@tcd.ie", "doctor", "")
+save_reference("12345","energy", "medical", "Energy", "Meta")
+
