3cfbad1009556bd6abbdb31eabd531c1a131ab92
Loke <92522067+LokeshSelvakumar@users.noreply.github.com>
2022-04-24 17:20:17 +0100

    questionnaire commit

--- Refrences/tg-bottest.py
+++ Refrences/tg-bottest.py
@@ -1,3 +1,4 @@
+from tkinter import E
 import telebot
 import yfinance as yf
 from yahoo_fin import stock_info as si
@@ -109,46 +110,106 @@ def check_portfolio(message):
     try:
         chat_id = message.chat.id
         text = message.text
+        user = user_dict[chat_id]
+        user.stock_invested = text
+        user.stocksList = []
+        user.watchList = []
         if text == 'Yes':
-            bot.send_message(chat_id, 'what are the stocks you have invested on? ')
-            bot.send_message(chat_id, 'Please refer the stock symbols below.')
+            msg = bot.reply_to(message, 'Lets create a portfolio for you \n\
+To add stock to your portfolio follow the format: <stock_symbol> <units>\n eg: amzn 100\n\
+Please refer the stock symbols in the link below')
             bot.send_message(chat_id,"https://www.slickcharts.com/sp500",parse_mode='HTML')
-            bot.send_message(chat_id,"To add stock to your portfolio follow the below format")
-            bot.send_message(chat_id, "stock_symbol units")
-            msg = bot.reply_to(message,'EG: amazn 100')
             bot.register_next_step_handler(msg, add_portfolio)
         elif text == 'No':
-            msg = bot.reply_to(message,'Nice to meet you')
-            bot.register_next_step_handler(msg,show_stock_price)
-        else:
-            msg = bot.reply_to(message, 'Have you invested in stocks?',reply_markup=markup)
-            bot.register_next_step_handler(msg, check_portfolio)
+            msg = bot.reply_to(message,"what are the stocks you wish to add to your watchlist?\n\
+Reply 'N' to skip this step or try adding symbol name one by one. Eg: amzn")
+            bot.register_next_step_handler(msg,add_watchList)
     except:
-        bot.sendMessage("problem with port folio")
+        bot.reply_to(message,"problem with port folio")
+        
 
 def add_portfolio(message):
     try:
         chat_id = message.chat.id
-        msg = bot.reply_to(message, 'to stop adding stock press N')
-        print(message.text)
-        print(message.text == 'N') 
+        user = user_dict[chat_id]
         if message.text != 'N':
-            bot.register_next_step_handler(msg, add_portfolio)
-        else:
-            #Ask next question
-            bot.register_next_step_handler(msg, show_stock_price)
-    except:
-        bot.sendMessage("problem with stock price")
+            print("printing before splti")
+            print(message.text.split()[0])
+            validationBool = validate_ticker(message.text.split()[0])
+            if validationBool:
+               user.stocksList.append(message.text)
+               bot.send_message(chat_id, 'stock added to portfolio')
+               msg = bot.reply_to(message, 'to stop adding stock press N')
+               bot.register_next_step_handler(msg, add_portfolio)
+            else:
+               msg = bot.reply_to(message, 'Stock Symbol is not valid. Try adding a valid one')
+               bot.register_next_step_handler(msg, add_portfolio)
 
-def show_stock_price(message):
-    try:
-        chat_id = message.chat.id
-        
-        bot.send_message(chat_id, "You can check the real-time price of any stocks. Try 'price ticker_name'")
-        bot.send_message(chat_id,"for stock symbol list please refer:")
-        bot.send_message(chat_id,"https://www.slickcharts.com/sp500",parse_mode='HTML')
+        else:
+            msg = bot.reply_to(message,"what are the stocks you wish to add to your watchlist?\n\
+Reply 'N' to skip this step or try adding symbol name one by one. Eg: amzn")
+            bot.register_next_step_handler(msg,add_watchList)
+#           
     except:
-        bot.sendMessage("problem with stock price")
+         bot.send_message("problem with stock price")
+
+
+def add_watchList(message):
+    try:
+        user = user_dict[message.chat.id]
+        if message.text != 'N':
+            validationBool = validate_ticker(message.text.split()[0])
+            if validationBool:
+               user.watchList.append(message.text)
+               msg = bot.reply_to(message, "to stop adding to watchList reply 'N'")
+               bot.register_next_step_handler(msg, add_watchList)
+            else:
+               msg = bot.reply_to(message, 'Stock Symbol is not valid. Try adding a valid one')
+               bot.register_next_step_handler(msg, add_watchList)
+
+        else:
+            markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+            markup.add('HealthCare', 'Tech','Automobile','Sports','Energy Source')
+            msg = bot.reply_to(message,"what is the sector you are interested in?", reply_markup=markup)
+            bot.register_next_step_handler(msg, save_sector)
+        
+    except:
+        bot.reply_to(message, "problem with watchList")
+
+def save_sector(message):
+    try:
+        print(message.text)
+        user = user_dict[message.chat.id]
+        user.sector = message.text
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add('Google','Yahoo')
+        msg = bot.reply_to(message,"what is your Source preference? ", reply_markup = markup)
+        bot.register_next_step_handler(msg, save_preference)
+    except:
+        bot.reply_to(message, "problem with save sector")
+
+def save_preference(message):
+    try:
+        user = user_dict[message.chat.id]
+        user.source_pref = message.text
+        msg = bot.reply_to(message, "You can check the real-time price of any stocks. Try 'price ticker_name'\n\
+for stock symbol list please refer:")
+        bot.send_message(message.chat.id,"https://www.slickcharts.com/sp500",parse_mode='HTML')
+        bot.register_next_step_handler(msg, get_stock_price)
+    except:
+        bot.reply_to(message, "problme with save_preference")
+        
+def validate_ticker(ticker_name):
+    ticker = yf.Ticker(ticker_name)
+    try:
+        if (ticker.info['regularMarketPrice'] == None):
+            print(f"Cannot get info of {ticker_name}, it probably does not exist")
+            return False
+    except:
+        print("error in validate ticker")
+    # Got the info of the ticker, do more stuff with it
+    print(f"Info of {ticker_name}: {ticker.info}")
+    return True
 
 def is_price_message(message):
     data = message.text.split()
@@ -156,10 +217,16 @@ def is_price_message(message):
         return False
     else:
         return True
-
 @bot.message_handler(func=is_price_message)
 def get_stock_price(message):
+    chat_id = message.chat.id
+    user = user_dict[chat_id]
+    print(user.stocksList)
     request_ticker = message.text.split()[1]
+    validBool = validate_ticker(request_ticker)
+    if not validBool:
+        msg = bot.reply_to(message,"stock symbol not found. Try again")
+        bot.register_next_step_handler(msg, get_stock_price)
     data = yf.download(tickers=request_ticker, period='5m', interval='1m')
     if data.size > 0:
         data = data.reset_index()
@@ -168,7 +235,7 @@ def get_stock_price(message):
         print(data.to_string())
         bot.reply_to(message, data['Close'].to_string(header=False))
     else:
-        bot.send_message(message, "No data.")   
+        bot.send_message(message, "No data.")  
 
 bot.enable_save_next_step_handlers(delay=2)
 
