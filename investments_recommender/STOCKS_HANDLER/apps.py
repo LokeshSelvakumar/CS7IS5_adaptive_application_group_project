b48b7b2606b9c3a58b7313d72cb68f24ec991b91
ritika24-s <24octritika@gmail.com>
2022-04-27 13:47:29 +0100

    schedulers

--- investments_recommender/STOCKS_HANDLER/apps.py
+++ investments_recommender/STOCKS_HANDLER/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class StocksHandlerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'STOCKS_HANDLER'
+
+    def ready(self):
+        from .scheduler import scheduler_gather_data
+        scheduler_gather_data()
