f4523be49255b4dbd0b258f450bdeb5b146ea961
Ciara Gilsenan <41157101+CiaraG98@users.noreply.github.com>
2022-04-28 13:12:51 +0100

    Delete bullfinch_adaptive_app directory

--- bullfinch_adaptive_app/lib/models/news_model.dart
+++
@@ -1,9 +0,0 @@
-class NewsModel{
-    String title = '';
-    String content = '';
-
-    NewsModel() {
-        title = "This is title";
-        content = "This is content";
-    }
-}
--- bullfinch_adaptive_app/lib/models/stocks_model.dart
+++
@@ -1,9 +0,0 @@
-class StocksModel{
-    String title = '';
-    String content = '';
-
-    StocksModel(Map<String, String> json) {
-      //title = json["title"];
-      //content = json['content'];
-    }
-}
